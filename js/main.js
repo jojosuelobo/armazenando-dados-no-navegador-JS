1931af34d303963ea95519427aae3a46d29a22b8
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-18 11:01:11 -0300

    Finalização topico 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log(document.getElementById("novoItem"))
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+
+form.addEventListener("submit", (evento) =>{
+    evento.preventDefault()
+    criarElementos(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criarElementos(nome, quantidade){
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild = (numeroItem)
+    novoItem.innerHTML = nome
+
+    lista.appendChild(novoItem)
+}
